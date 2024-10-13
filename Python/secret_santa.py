1b25259b46263779d69f53b03b0107dcf8b6d6aa
Gustav Abrahamsson <gustav.abrahamsson@outlook.com>
2024-10-13 18:10:08 +0200

    2024 push. Add check, fix file saving.

--- Python/secret_santa.py
+++ Python/secret_santa.py
@@ -6,23 +6,30 @@ from operator import length_hint
 import random
 import numpy as np
 
-year = "2023"
+year = "2024"
 
-link = "https://secret-santa-viktor-gustav.vercel.app/"
+link = "https://secret-santa-viktor-gustav.vercel.app"
 
 Person = namedtuple('Person', ['name', 'family', 'given_to'])
 
-#                                                  2021      2022
-people = [  Person("Johan",   "Knutsson",       {"Gustav",  "Oscar"     }), 
-            Person("Oscar",   "Knutsson",       {"Carina",  "Hakan"     }), 
-            Person("Samuel",  "Knutsson",       {"Hakan",   "Matilda"   }),
-            Person("Carina",  "Knutsson",       {"Viktor",  "Linda"     }), 
-            Person("Anders",  "Knutsson",       {"Samuel",  "Carina"    }), 
-            Person("Viktor",  "Abrahamsson",    {"Oscar",   "Anders"    }),
-            Person("Gustav",  "Abrahamsson",    {"Linda",   "Viktor"    }), 
-            Person("Linda",   "Abrahamsson",    {"Anders",  "Gustav"    }), 
-            Person("Hakan",   "Abrahamsson",    {"Johan",   "Samuel"    })]
-           # Person("Matilda", "Knutsson",       {"Oscar",   "Johan"     })]
+#                                                  2021      2022        2023
+people = [  Person("Johan",   "Knutsson",       {"Gustav",  "Oscar",    "Anders"}), 
+            Person("Oscar",   "Knutsson",       {"Carina",  "Hakan",    "Linda"}), 
+            Person("Samuel",  "Knutsson",       {"Hakan",   "Johan",    "Viktor"}),
+            Person("Carina",  "Knutsson",       {"Viktor",  "Linda",    "Gustav"}), 
+            Person("Anders",  "Knutsson",       {"Samuel",  "Carina",   "Oscar"}), 
+            Person("Viktor",  "Abrahamsson",    {"Oscar",   "Anders",   "Hakan"}),
+            Person("Gustav",  "Abrahamsson",    {"Linda",   "Viktor",   "Samuel"}), 
+            Person("Linda",   "Abrahamsson",    {"Anders",  "Gustav",   "Johan"}), 
+            Person("Hakan",   "Abrahamsson",    {"Johan",   "Samuel",   "Carina"}),
+            Person("Frida",   "Knutsson",       {"Samuel",  "Samuel",   "Samuel"})]
+
+
+
+file_name = "Results_" + year + ".txt"
+open(file_name, 'w').close()
+f = open(file_name, "a")
+
 
 random.shuffle(people)
 
@@ -69,6 +76,8 @@ def decrypt(name):
 
 def generate_secret_santas(people_list):
 
+   output = []
+
    num_same_fam = 0
    num_people = 0
 
@@ -98,42 +107,94 @@ def generate_secret_santas(people_list):
             num_same_fam += 1
 
          encrypted_recipient = encrypt(target_person.name)
-         #print(person.name + " --> " + encrypted_recipient + " | Last year: " + person.given_to + showDecrypted * (" | Decrypted: " + decrypt(encrypted_recipient)))
+         # print(person.name + " --> " + encrypted_recipient + " | Last year: " + list(person.given_to)[-1] + showDecrypted * (" | Decrypted: " + decrypt(encrypted_recipient)))
          people_list2.remove(target_person)
 
          num_people += 1
 
-         print("Hej " + person.name + "!\n\n" + 
-                "Nu ska du få reda på vem du ska ge i julklappsleken " + year + ".\n" +
-                "Du ska gå in på: " + link + " och mata in koden som kommer i nästa meddelande.\n" +
-                "Om du får den du fick förra året eller om andra problem uppstår har du hittat en bug, säg till då!\n\n" +
-                "God jul!\n\n\n" +
-                 encrypted_recipient + "\n\n\n\n")
+         output.append((person.name, target_person.name, encrypted_recipient))
+
+         # print("Hej " + person.name + "!\n\n" + 
+         #        "Nu ska du få reda på vem du ska ge i julklappsleken " + year + ".\n" +
+         #        "Du ska gå in på: " + link + " och mata in koden som kommer i nästa meddelande.\n" +
+         #        "Om du får den du fick förra året eller om andra problem uppstår har du hittat en bug, säg till då!\n\n" +
+         #        "God jul!\n\n\n" +
+         #         encrypted_recipient + "\n\n\n\n")
 
          break
 
-   return num_same_fam, num_people
+   return output, num_same_fam, num_people
+
+# Print one message
+def print_message(gifter, receiver, encrypted_receiver):
+   message = "Hej " + gifter + "!\n\n" + \
+                "Nu ska du få reda på vem du ska ge i julklappsleken " + year + ".\n" + \
+                "Du ska gå in på: " + link + " och mata in koden som kommer i nästa meddelande.\n" + \
+                "Om du får den du fick förra året eller om andra problem uppstår, hör av dig!\n\n" + \
+                "God jul!\n\n\n" + \
+                 encrypted_receiver + "\n\n\n\n"
+   print(message)
+
+   return message
+
+def print_all_messages(gift_list):
+   for transaction in gift_list:
+      msg = print_message(transaction[0], transaction[1], transaction[2])
+      f.write(msg)
+
+   f.close()
+
+
+def check_output(gift_list):
+   for transaction in gift_list:
+      for person in people:
+         if person.name == transaction[0]:
+            #assert transaction[1] not in person.given_to
+            if transaction[1] in person.given_to:
+               print(" ======================== ERROR ========================")
+               return
+               
+   print("All checks OK!\n")
+   # f.write("All checks OK!\n\n")
+   
 
 
 # Generate secret santa with a tolerance "[min, max]" of the number of people who give to their own family members
 def generate(min, max):
+   gift_list = []
    num_same = 0
    num_people = 0
    while (num_same < min or num_same > max or num_people != len(people)):
-      print("------------------------------------------------------------------------")
-      num_same, num_people = generate_secret_santas(people)
+      gift_list, num_same, num_people = generate_secret_santas(people)
+
+   check_output(gift_list)
+
+   print_all_messages(gift_list)
+
+   
 
    #print("Found a 'num_same' = " + str(num_same))
    print("Number of people = " + str(num_people))
 
+print("IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII")
+
+print("\n")
+print("\n")
+
 generate(2, 3)
 
-
+print("\n")
+print("\n")
+print("\n")
+print("\n")
+print("\n")
+print("\n")
+print("\n")
 #generate_secret_santas(people)
 
 
 # # Testing
-num_same = []
+# num_same = []
 # for i in range(1,100):
 #    num_same.append(generate_secret_santas(people))
 
@@ -183,9 +244,9 @@ num_same = []
 
 # print("\n" + decrypt("ijHfwnsfrpja^jp"))
 
-#for person in people:
-    #encrypt(person)
-    #print("\n")
+# for person in people:
+#     encrypt(person)
+#     print("\n")
 
 
 
